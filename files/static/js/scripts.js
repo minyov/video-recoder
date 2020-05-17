2f7eef8803a547f3cde7acab9aa2645256b8bf32
Vladimir Minev <vladimir@outlinestudio.co>
2020-05-17 20:42:26 +0300

    Add ajax method to file uploader. Add alerts

--- files/static/js/scripts.js
+++ files/static/js/scripts.js
@@ -1,4 +1,30 @@
 $(document).ready(function () {
+    $("#file-submit").on('click', function () {
+        var fd = new FormData();
+        var files = $('#file-input')[0].files[0];
+        fd.append('file', files);
+
+        $.ajax({
+            url: '/v1/videos/',
+            type: "POST",
+            data: fd,
+            contentType: false,
+            processData: false,
+            headers: {
+                'X-CSRFToken': $("input[name='csrfmiddlewaretoken']").val()
+            },
+            success: function (result) {
+                alert('Файл отправлен')
+                window.location.reload()
+            },
+            error: function (xhr, resp, text) {
+                alert('Ошибка отправки файла')
+            },
+            complete: function () {
+                $("input[name='url']").val('')
+            }
+        })
+    });
     $("#url-submit").on('click', function () {
         var data = {}
         $("#url-form").serializeArray().forEach(function (elem) {
@@ -14,10 +40,14 @@ $(document).ready(function () {
             },
             data: JSON.stringify(data),
             success: function (result) {
-                console.log(result);
+                alert('Ссылка в обработке')
+                window.location.reload()
             },
             error: function (xhr, resp, text) {
-                console.log(xhr, resp, text);
+                alert('Ошибка отправки ссылки')
+            },
+            complete: function () {
+                $("input[name='url']").val('')
             }
         })
     });
