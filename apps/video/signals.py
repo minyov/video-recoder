7ec565fd75b762bd3c795358e578fc73eb243fd4
Vladimir Minev <vladimir@outlinestudio.co>
2020-05-17 17:26:13 +0300

    Add video_file_downloaded_by_url_post_save signal and refactor signals.py

--- apps/video/signals.py
+++ apps/video/signals.py
@@ -2,14 +2,26 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from .models import Video
-from .tasks import create_preview, recode_video
+from .tasks import create_preview, recode_video, download_video_file
 from .enums import VideoMimeTypeEnum
 
 
 @receiver(post_save, sender=Video)
 def video_post_save(instance: Video, created: bool, **kwargs):
     if created:
-        create_preview.delay(video_id=instance.id)
-        recode_video.delay(video_id=instance.id, ext=VideoMimeTypeEnum.WEBM.value)
-        recode_video.delay(video_id=instance.id, ext=VideoMimeTypeEnum.MP4.value)
+        if instance.file:
+            start_processing_video(instance.id)
+        elif instance.url:
+            download_video_file.delay(video_id=instance.id)
 
+
+@receiver(post_save, sender=Video)
+def video_file_downloaded_by_url_post_save(instance: Video, created: bool, **kwargs):
+    if all((not created, instance.file, instance.url)):
+        start_processing_video(instance.id)
+
+
+def start_processing_video(video_id: int):
+    create_preview.delay(video_id=video_id)
+    recode_video.delay(video_id=video_id, ext=VideoMimeTypeEnum.WEBM.value)
+    recode_video.delay(video_id=video_id, ext=VideoMimeTypeEnum.MP4.value)
